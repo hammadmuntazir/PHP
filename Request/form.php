e40cea6e7f20e848a619591db3b21b8f8a8f5eac
Hammad Muntazir <hammadmuntazir58@gmail.com>
2026-01-27 23:10:45 +0500

    Create form.php for user input form

---
+++ Request/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Form using Request </title>
+    </head>
+<body>
+    <form action="testform.php" method="post">
+        <label for="username"> Username: </label>
+        <input type="text" name="username" id="username" > <br> <br>
+
+        <label for="age"> Age: </label>
+        <input type="number" name="age" id="age" > <br> <br>
+
+        <input type="submit" value=" Submit ">
+    </form>
+</body>
+</html>
