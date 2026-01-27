5fd6d131afc4a0e0d1169ecfbf136ac8abd47230
Hammad Muntazir <hammadmuntazir58@gmail.com>
2026-01-27 23:08:13 +0500

    Add initial form page in GET/form.php

---
+++ GET/form.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Form Page</title>
+    </head>
+    <body>
+        <form action="testform.php" method="get">
+            Name:<input type="text" name="fname"><br><br>
+            Age:<input type="text" name="age"><br><br>
+            <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
