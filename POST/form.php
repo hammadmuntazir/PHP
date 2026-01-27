24ec8341ff32198249464b3c8b6e2414c03ccb58
Hammad Muntazir <hammadmuntazir58@gmail.com>
2026-01-27 23:09:36 +0500

    Create a POST form for user input

---
+++ POST/form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Post Form Page </title>
+</head>
+<body>
+    <form action="testform.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username"><br><br>
+        <label for="age">Age:</label>
+        <input type="text" id="age" name="age"><br><br>
+        <input type="submit" value="Submit">
+</form>
