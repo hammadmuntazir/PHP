612e82e50fb72e65bc18e0dc13160d479d2e48f3
Hammad Muntazir <hammadmuntazir58@gmail.com>
2026-01-27 23:10:11 +0500

    Add files via upload

---
+++ POST/testform.php
@@ -0,0 +1,7 @@
+<?php 
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
+echo"<h2 style='color:red';> Welcome ".$_GET['fname']."</h2>";
+// data url mein nazar aye ga aur  isy $_GET ko use krky show bi krva skty hain
+?>
